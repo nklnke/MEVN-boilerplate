d5b634ae185348c3e092896aac7329b50d0555dd
Danil Kozlov <megacokes@gmail.com>
2020-03-30 20:34:28 +0500

    Delete shiet

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="posts">
-    <h1>Posts</h1>
-    <div v-if="posts.length > 0" class="table-wrap">
+    <div class="table-wrap">
       <div>
         <router-link v-bind:to="{ name: 'addpost' }" class="">Add Post</router-link>
       </div>
@@ -9,21 +8,17 @@
         <tr>
           <td>Title</td>
           <td width="550">Description</td>
-          <td width="100" align="center">Action</td>
+          <td width="100">Action</td>
         </tr>
         <tr v-for="post in posts">
           <td>{{ post.title }}</td>
           <td>{{ post.description }}</td>
-          <td align="center">
+          <td>
             <router-link v-bind:to="{ name: 'editpost', params: { id: post._id } }">Edit</router-link> |
             <a href="#" @click="deletePost(post._id)">Delete</a>
           </td>
         </tr>
       </table>
-    </div>
-    <div v-else>
-      There are no posts.. Lets add one now <br /><br />
-      <router-link v-bind:to="{ name: 'addpost' }" class="add_post_link">Add Post</router-link>
     </div>
   </div>
 </template>
@@ -69,19 +64,9 @@ export default {
 .table-wrap {
   width: 60%;
   margin: 0 auto;
-  text-align: center;
-}
-table th, table tr {
-  text-align: left;
-}
-table thead {
-  background: #f2f2f2;
 }
 table tr td {
   padding: 10px;
-}
-table tr:nth-child(odd) {
-  background: #f2f2f2;
 }
 table tr:nth-child(1) {
   background: #4d7ef7;
